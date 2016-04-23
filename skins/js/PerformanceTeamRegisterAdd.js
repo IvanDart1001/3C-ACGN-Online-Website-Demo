511981d592f56b209c741b62c8d494ab1b42ac32
Ivan-the-Gazer <IvanDart1001@outlook.com>
2016-04-23 19:37:59 +0800

    Revert "Source"
    This reverts commit 46d2d03316cefff3f2c3859ab98aef03ec18bedf.

--- skins/js/PerformanceTeamRegisterAdd.js
+++
@@ -1,7 +0,0 @@
-﻿function addElementDiv(obj) {
-    var parent = document.getElementById(obj);
-    var div = document.createElement("div");
-    div.setAttribute("class", "CCC-activityPublish-Block");
-    div.innerHTML = "<select name='style' class='form-control CCC-activityPublish-select' style='margin-left:105px;'><option value=''>团队职务</option><option value='leader'>团长</option><option value='member'>组员</option><option value='cameraman'>摄影</option><option value='customizer'>化妆</option><option value='npc'>场务</option><option value='other'>其他</option></select><div class='form-group CCC-activityPublish-Block-Container  CCC-group-text'><input type='text' id='CCC-groupN' class='form-control CCC-activityPublish-input' /><span id='CCC-groupN-AT' class='CCC-activityPublish-inputPriceTag'>@</span></div>";
-    parent.appendChild(div);
-}
