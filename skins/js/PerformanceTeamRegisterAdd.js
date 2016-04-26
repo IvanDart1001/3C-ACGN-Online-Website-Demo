b7547d9e607da2f3996657647694713c630f950c
Ivan-the-Gazer <IvanDart1001@outlook.com>
2016-04-26 23:57:17 +0800

    Add form for information update.

--- skins/js/PerformanceTeamRegisterAdd.js
+++ skins/js/PerformanceTeamRegisterAdd.js
@@ -2,6 +2,10 @@
     var parent = document.getElementById(obj);
     var div = document.createElement("div");
     div.setAttribute("class", "CCC-activityPublish-Block");
-    div.innerHTML = "<select name='style' class='form-control CCC-activityPublish-select' style='margin-left:105px;'><option value=''>团队职务</option><option value='leader'>团长</option><option value='member'>组员</option><option value='cameraman'>摄影</option><option value='customizer'>化妆</option><option value='npc'>场务</option><option value='other'>其他</option></select><div class='form-group CCC-activityPublish-Block-Container  CCC-group-text'><input type='text' id='CCC-groupN' class='form-control CCC-activityPublish-input' /><span id='CCC-groupN-AT' class='CCC-activityPublish-inputPriceTag'>@</span></div>";
+    div.innerHTML = "<select name='style' class='form-control CCC-activityPublish-select' style='margin-left:105px;'><option value=''>团队职务</option><option value='leader'>团长</option><option value='member'>组员</option><option value='cameraman'>摄影</option><option value='customizer'>化妆</option><option value='npc'>场务</option><option value='other'>其他</option></select><div class='form-group CCC-activityPublish-Block-Container  CCC-group-text'><form style='float:left'><input type='text' id='CCC-groupN' class='form-control CCC-activityPublish-input' /><span id='CCC-groupN-AT' class='CCC-activityPublish-inputPriceTag'>@</span></form><button class='CCC-groupDEL-btn' onclick='cls(this)'>x</button></div>";
     parent.appendChild(div);
+}
+
+function cls(ob) {
+    ob.parentNode.parentNode.parentNode.removeChild(ob.parentNode.parentNode);
 }
