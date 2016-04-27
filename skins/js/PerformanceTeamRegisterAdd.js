a2b90c13469d244cdf9fc9ce194a784e1b02f8b6
Ivan-the-Gazer <IvanDart1001@outlook.com>
2016-04-27 21:10:16 +0800

    form complete

--- skins/js/PerformanceTeamRegisterAdd.js
+++ skins/js/PerformanceTeamRegisterAdd.js
@@ -1,11 +1,15 @@
-﻿function addElementDiv(obj) {
+﻿var count = 1
+
+function addElementDiv(obj) {
     var parent = document.getElementById(obj);
     var div = document.createElement("div");
     div.setAttribute("class", "CCC-activityPublish-Block");
-    div.innerHTML = "<select name='style' class='form-control CCC-activityPublish-select' style='margin-left:105px;'><option value=''>团队职务</option><option value='leader'>团长</option><option value='member'>组员</option><option value='cameraman'>摄影</option><option value='customizer'>化妆</option><option value='npc'>场务</option><option value='other'>其他</option></select><div class='form-group CCC-activityPublish-Block-Container  CCC-group-text'><form style='float:left'><input type='text' id='CCC-groupN' class='form-control CCC-activityPublish-input' /><span id='CCC-groupN-AT' class='CCC-activityPublish-inputPriceTag'>@</span></form><button class='CCC-groupDEL-btn' onclick='cls(this)'>x</button></div>";
+    div.innerHTML = "<select name='MemberJob" + count + "' class='form-control CCC-activityPublish-select' style='margin-left:105px;'><option value=''>团队职务</option><option value='leader'>团长</option><option value='member'>组员</option><option value='cameraman'>摄影</option><option value='customizer'>化妆</option><option value='npc'>场务</option><option value='other'>其他</option></select><div class='form-group CCC-activityPublish-Block-Container  CCC-group-text'><input name='MemberID" + count + "' type='text' id='CCC-groupN' class='form-control CCC-activityPublish-input' /><span id='CCC-groupN-AT' class='CCC-activityPublish-inputPriceTag'>@</span><button class='CCC-groupDEL-btn' type='button' onclick='cls(this)'>x</button></div>";
+    count++;
     parent.appendChild(div);
 }
 
 function cls(ob) {
+    count--;
     ob.parentNode.parentNode.parentNode.removeChild(ob.parentNode.parentNode);
 }
